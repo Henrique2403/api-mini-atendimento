b60b9afba4a028d168f4720aade1071b1a60f973
Henrique2403 <henriquecc6245@gmail.com>
2025-08-22 17:19:51 -0300

    feat: logging básico com Serilog

--- api-mini-atendimento/api-mini-atendimento/Program.cs
+++ api-mini-atendimento/api-mini-atendimento/Program.cs
@@ -1,3 +1,5 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+builder.Host.UseSerilog();
 
 var app = builder.Build();
 
@@ -21,5 +26,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseSerilogRequestLogging();
 
 app.Run();
