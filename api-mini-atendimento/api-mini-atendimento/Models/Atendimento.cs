6d97a8d112edb77a0b801b3a30229121af5420ed
Henrique2403 <henriquecc6245@gmail.com>
2025-08-22 17:25:21 -0300

    feat: modelos Cliente e Atendimento

---
+++ api-mini-atendimento/api-mini-atendimento/Models/Atendimento.cs
@@ -0,0 +1,11 @@
+﻿namespace api_mini_atendimento.Models;
+
+public class Atendimento
+{
+    public int Id { get; set; }
+    public string Canal { get; set; } = string.Empty;
+    public string Mensagem { get; set; } = string.Empty;
+    public DateTime Data { get; set; }
+    public required Cliente ClienteId { get; set; }
+    public required Cliente Cliente { get; set; }
+}
